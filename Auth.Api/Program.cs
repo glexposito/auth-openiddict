47ed0dab418cb36fc2ce0d58ad4047ab816ce4c0
Guillermo <glexposito@gmail.com>
2023-06-05 19:01:52 +1200

    Update Program.cs

--- Auth.Api/Program.cs
+++ Auth.Api/Program.cs
@@ -17,6 +17,14 @@ ConfigureOpenIddict(builder.Services);
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<ApplicationDbContext>();
+
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
